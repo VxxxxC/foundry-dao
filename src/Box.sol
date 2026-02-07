abffebe290c6b1a8ff7a890cd5ab289cb4c1f48e
vxxxxc <veper1234@icloud.com>
2026-02-07 23:53:04 +0800

    install openzeppeline-contracts lib & created Box contract

---
+++ src/Box.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BOX is Ownable(msg.sender) {
+    uint256 private s_number;
+
+    event NumberChanged(uint256 number);
+
+    function store(uint256 _number) public onlyOwner {
+        s_number = _number;
+        emit NumberChanged(_number);
+    }
+
+    function getNUmber() public view returns (uint256) {
+        return s_number;
+    }
+}
